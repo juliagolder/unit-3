cbfb72a88ebdf787313e27083e6ea708b42f8dda
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-02-14 08:37:30 -0500

    Updated from Brython Server: 2/14/2018 8:37:30 AM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,11 @@
+#julia golder
+#2/14/18
+#favorites.py - how to loop a word
 
+word = input('Enter your favorite word')
+number = int(input('Enter yor favorite number'))
+
+i = 0
+while i<number:
+    print(word)
+    i = i+1
