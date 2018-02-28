119a45482e77dcc25f7904b02f416fd905d574d5
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-02-28 08:16:41 -0500

    Update betterAdditionGameDemo.py

--- betterAdditionGameDemo.py
+++ betterAdditionGameDemo.py
@@ -1,1 +1,12 @@
+#julia golder
+#1/28/18
+#betterAdditionGameDemo.py - more loop practice
 
+from random import randint
+
+numCorrect = 0
+while numCorrect is < 5:
+    num1 = randint(-10,10)
+    num2 = randint(-10,10)
+    input('What is ' + str(num1) + '+' + str(num2) + '? ')
+    
