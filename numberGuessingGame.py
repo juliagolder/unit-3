5c8813a801f9d675551c9cdf4278b6f3864f1028
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-03-01 08:46:05 -0500

    Updated from Brython Server: 3/1/2018 8:46:06 AM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,1 +1,16 @@
+#julia golder
+#3/1/18
+#numberGuessingGame.py
 
+text = int(input('Guess a number between 1 and 100: '))
+
+import from randint(1,100)
+
+if num == 64:
+    print('This is correct!')
+elif num < 64
+    print('This is too high!')
+elif num > 64
+    print('This is too low!')
+else:
+    print('This is odd!')
