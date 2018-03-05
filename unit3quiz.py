39051d40abb4aab1938c1790a714949f9816a7f9
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-03-05 08:44:58 -0500

    Updated from Brython Server: 3/5/2018 8:45:00 AM

--- unit3quiz.py
+++ unit3quiz.py
@@ -1,1 +1,24 @@
+#julia golder
+#3/5/18
+#unit3quiz.py
 
+
+
+i = -15
+while i<= -9:
+    print(i)
+    i += 1
+
+for i in range(50,17,-4):
+    print(i)
+
+total = 0
+for i in range(-100,1000,2):
+    total = total + i
+print(total)
+
+while True:
+    text = input('Enter text: ')
+    if text == 'alpaca':
+        break
+    print(text)
