e08dd7c78210a11c37bec906927f90d64164e51e
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-03-04 22:56:09 -0500

    Update unit3Practice.py

--- unit3Practice.py
+++ unit3Practice.py
@@ -1,1 +1,92 @@
 
+"""
+for i in range(-10,15,4)
+    print(i+1)
+"""
+
+"""
+for i in range(50,30,-2):
+    if i%4 == 0:
+        print(i)
+"""
+
+"""
+num = 7
+while num <15:
+    num+= 2
+print(num)
+"""
+
+"""
+i = 1
+while i<100:
+    print(i)
+    i*=2
+"""
+
+"""
+for ch in 'grape':
+    print(ch*3)
+"""
+
+"""
+total = 0
+n = 5
+while n<10:
+    total += n
+    n = n+1
+print(total)
+"""
+
+"""
+for i in range(1,4):
+    print(i*100)
+#mulitplies by 100
+"""
+
+"""
+n=1
+while n<=4:
+    print('#'*n)
+    n=n+1
+#prints hashtags
+"""
+
+"""
+for ch in 'abcde'
+    print(ch.upper())
+#prints uppercase
+"""
+
+"""
+num1 = int(input('Enter starting integer: '))
+num2 = int(input('Enter ending integer: '))
+
+total = 0
+
+for: in range(num1,num2+1)
+    total += i
+print('The sum is', total)
+#a code that asks for starting andending integer then prints sum of all integers from start to stop
+#ex: if the user puts in 1 and 5, the code should output 15
+"""
+
+"""
+name = input('Enter your name: ')
+
+while True
+    print(name)
+
+#code that prints name forever
+"""
+
+"""
+num = int(input('Enter a number between 2 and 12: '))
+i = num
+while i<= 100:
+    print(i)
+    i+-num
+#prints out all multiples of that number through 100
+"""
+
+
